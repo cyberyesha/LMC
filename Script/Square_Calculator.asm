70421fe84a5e3e823c3dc6faa427be92f28d874a
yesha <107079162+cyberyesha@users.noreply.github.com>
2023-11-22 05:08:43 +0000

    Create Square_Calculator.asm

---
+++ Script/Square_Calculator.asm
@@ -0,0 +1,17 @@
+// Square Calculator
+// This script takes a user-input number and calculates its square.
+// The program multiplies the input by itself, and the result is output to the console.
+// Date; Nov 11th 2023
+// Created by; yesha
+
+        INP
+        STA VALUE
+        LDA VALUE
+        ADD VALUE
+        STA RESULT
+        OUT
+        HLT
+VALUE   DAT
+RESULT  DAT
+
+// End of Square Calculator
