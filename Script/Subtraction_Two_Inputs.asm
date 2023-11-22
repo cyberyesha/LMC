b5e641b5e05bba7b700da1d443a031446634458e
yesha <107079162+cyberyesha@users.noreply.github.com>
2023-11-22 01:55:58 +0000

    Create Subtraction_Two_Inputs.asm

---
+++ Script/Subtraction_Two_Inputs.asm
@@ -0,0 +1,17 @@
+// Subtraction Script
+// This script takes two user inputs and then subtracts them.
+// and outputs the result to the console. The final result is stored in memory address 50.
+// The code uses input (INP), subtraction (SUB), storing values (STA), output (OUT),
+// and halting the program (HLT) instructions to perform the subtraction operation.
+// Date; Nov 11th 2023
+// Created by; yesha
+
+INP
+STA 50
+INP
+SUB 50
+STA 50
+OUT
+HLT
+
+// End of Subtraction Program
