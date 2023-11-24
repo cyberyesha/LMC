38b7e483d3366bc7e92e78001ccbb6dac88a23f4
yesha <107079162+cyberyesha@users.noreply.github.com>
2023-11-24 23:51:28 +0000

    Update Fibonacci_Sequence_Generator.asm

--- Script/Fibonacci_Sequence_Generator.asm
+++ Script/Fibonacci_Sequence_Generator.asm
@@ -1,1 +1,44 @@
+# Fibonacci Sequence Generator
+# This script takes user input for the number of Fibonacci terms to generate and outputs the corresponding sequence. It includes error handling for invalid input.
+# Date; Nov 24th 2023
+# Created by; yesha
 
+        INP
+        STA TERMS
+        LDA TERMS
+        BRZ END
+        BRP VALID
+        BRA INVALID
+VALID   STA COUNT
+        LDA COUNT
+        STA FIRST
+        OUT
+        LDA COUNT
+        ADD ONE
+        STA COUNT
+        LDA COUNT
+        SUB TERMS
+        BRZ END
+        BRP LOOP
+        BRA INVALID
+LOOP    LDA SECOND
+        ADD FIRST
+        STA RESULT
+        OUT
+        LDA FIRST
+        STA SECOND
+        LDA RESULT
+        STA FIRST
+        BRA VALID
+END     HLT
+INVALID OUT INVALID_MSG
+        HLT
+TERMS   DAT
+COUNT   DAT
+FIRST   DAT
+SECOND  DAT
+RESULT  DAT
+ONE     DAT 001
+INVALID_MSG DAT 999
+
+# End of Fibonacci Sequence Generator Program
